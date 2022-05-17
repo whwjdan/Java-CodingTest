5992e346cbe25a7f1c1ac1daf2d5dfa11357b14a
whwjdan <35955189+whwjdan@users.noreply.github.com>
2022-05-17 20:29:05 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.81 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    // 번호를 누른 후 새롭게 누른 번호의 좌표를 반환
     public int [] find_idx(int number, int [][] keyPads){
         int [] return_val = new int[2];
         for(int i=0; i<3; i++){
@@ -7,7 +9,6 @@ class Solution {
                     return_val[0] = 3;
                     return_val[1] = 1;
                 }
-                
                 else if(number == keyPads[i][j]){
                     return_val[0] = i;
                     return_val[1] = j;
@@ -16,59 +17,45 @@ class Solution {
         }
         return return_val;
     }
-    
+    // 규칙 4의 경우 거리를 측정후 누르는 숫자를 리턴
     public String find_distance(int [] l_idx, int [] r_idx, int [] p_idx ,int number){
         int l_distance = Math.abs(p_idx[0]-l_idx[0]) + Math.abs(p_idx[1]-l_idx[1]);
         int r_distance = Math.abs(p_idx[0]-r_idx[0]) + Math.abs(p_idx[1]-r_idx[1]);
-        System.out.println("find_distance");
-        System.out.println(l_distance);
-        System.out.println(r_distance);
-        System.out.println("--------");
-        System.out.println(p_idx[0]);
-        System.out.println(p_idx[1]);
-        System.out.println("p--------");
+
         if(l_distance < r_distance){
             return "L";
         } else if(l_distance > r_distance){
             return "R";
         } else{
             return "E";
-        }
-        
+        }   
     }
-    
     public String solution(int[] numbers, String hand) {
         String answer = "";
         
         //String [][] keyPads = {{"1","2","3"},{"4","5","6"},{"7","8","9"},{"*","0","#"}};
         // 10 : *  /  11 : #
         int [][] keyPads = {{1,2,3},{4,5,6},{7,8,9},{10,0,11}};
+        // l_idx = 왼손가락의 좌표, r_idx = 오른손가락의 좌표, p_idx = 눌러야 할 숫자의 좌표
         int [] l_idx = {3,0};
         int [] r_idx = {3,2};
         int [] p_idx = {0,0};
-        System.out.println("ddddddddd");
-        System.out.println(find_idx(0, keyPads)[0]);
+        
         for(int number : numbers){
             if(number == 1 || number == 4 || number ==7){
                 answer+=("L");
                 l_idx = find_idx(number, keyPads);
-                System.out.println("77777777");
             } else if(number == 3 || number == 6 || number == 9){
                 answer+=("R");
                 r_idx = find_idx(number, keyPads);
-                //System.out.println(r_idx[1]);
             } else {
-                System.out.println("0000000");
                 p_idx = find_idx(number, keyPads);
                 String dist = find_distance(l_idx, r_idx, p_idx, number);
-                System.out.println(dist);
                 if(dist == "E"){
                     if(hand.equals("left")){
-                        System.out.println("LLLLL");
                         answer+=("L");
                         l_idx = p_idx;
                     } else if(hand.equals("right")){
-                        System.out.println("RRRRR");
                         answer+=("R");
                         r_idx = p_idx;
                     }
@@ -81,9 +68,6 @@ class Solution {
                 }
             }
         }
-        
-        
-        
         return answer;
     }
 }
