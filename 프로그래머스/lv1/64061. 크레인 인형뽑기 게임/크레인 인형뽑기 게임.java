b21bd777d8ac2a4bd8a2c3d6088bdfdd46c3c1be
whwjdan <35955189+whwjdan@users.noreply.github.com>
2022-05-21 17:47:05 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 14.29 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    
+    Stack <Integer> baguni = new Stack<>();
+    
+    static int answer = 0;
+    public int solution(int[][] board, int[] moves) {
+        for(int move: moves){
+            System.out.println("move : "+ (move-1));
+            //board[move-1] = moving(board[move-1]);
+            //moving(boards[move-1]);
+            for(int j=0; j<board.length; j++){
+                if(board[j][move-1] != 0){
+                    
+                    if(!baguni.isEmpty() && baguni.peek() == board[j][move-1]){
+                        answer+=2;
+                        baguni.pop();
+                    } else{
+                        baguni.add(board[j][move-1]);    
+                    }
+                    
+                    board[j][move-1] = 0;
+                    break;
+                }
+                    
+            }
+        }
+        return answer;
+    }
+    
+    
+}
+
+/**
+n by n 크기
+아래부터 쌓임
+바구니는 따로
+같은인형 2개는 터진다
+**/
