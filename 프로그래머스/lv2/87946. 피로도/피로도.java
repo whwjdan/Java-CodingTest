e8dc2b93ffcbcc58f5a5a153cc2f2d0486ac9b2c
whwjdan <35955189+whwjdan@users.noreply.github.com>
2022-12-03 18:20:28 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    
+    static boolean [] visited;
+    int answer = 0;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean [dungeons.length];
+        permutation(k, dungeons, 0);
+        return answer;
+    }
+    
+    public void permutation(int piro, int[][] dungeons, int depth){
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i] && piro >= dungeons[i][0]){
+                visited[i] = true;
+                //System.out.println("현재 피로도는 " + piro);
+                //System.out.println("최소 피로도는 " + dungeons[i][0]);
+                //System.out.println("방문 피로도는 " + dungeons[i][1]);
+                //System.out.println("줄어든 현재 피로도는 " + piro);
+                permutation(piro - dungeons[i][1], dungeons, depth+1);  
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+        //System.out.println("증가한 현재 피로도는 " + piro);
+        
+    }
+}
